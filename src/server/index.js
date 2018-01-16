c9e0bf8b118e3ab05e5965f857e76257fee80619
TanguyDeTaxis <tanguy.detaxis@gmail.com>
2018-01-16 16:11:58 +0100

    Add express launcher

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const api = require('./api.js');
 const config = require('./config.js');
+
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('mille xD')
+})
+
+app.get('/coucou', function (req, res) {
+    res.send('TU MA DIS COUCOU C DROLE CA!')
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
