e45a816a76d144c8f5d89e58a1a1bb5635bdfe35
TanguyDeTaxis <tanguy.detaxis@gmail.com>
2018-01-23 16:29:01 +0100

    Send json on /episodes

--- src/server/api.js
+++ src/server/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var fs = require("fs")
 
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
@@ -9,7 +10,9 @@ router.use(function timeLog(req, res, next) {
 });
 // define the home page route
 router.get('/', function(req, res) {
-    res.send('Hello world!');
+    var file = fs.readFileSync("data/episodes.json", "utf-8");
+    var json = JSON.parse(file);
+    res.send(json);
 });
 
 router.post('/', function (req, res) {
