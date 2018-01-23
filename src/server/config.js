311dbc17eda0ce9f5db3f075b94bd475bd46490f
Ozcan SEVIK <sevik.ozcan@gmail.com>
2018-01-23 16:02:52 +0100

    fix last errors

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const config = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 4000,
     static: process.env.STATIC || path.join(__dirname, '/../../build'),
     data: process.env.DATA || path.join(__dirname, '/../../data')
 }
