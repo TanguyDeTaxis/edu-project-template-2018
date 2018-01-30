1fcea2fca7babbfe80bd1fb481fd73dc815bb0ee
Ozcan SEVIK <sevik.ozcan@gmail.com>
2018-01-30 15:07:28 +0100

    add delete api

--- src/server/api.js
+++ src/server/api.js
@@ -10,45 +10,56 @@ router.use(function timeLog(req, res, next) {
     next();
 });
 // define the home page route
-router.get('/', function(req, res) {
-   var fileReaded = [];
+router.get('/', function (req, res) {
+    var fileReaded = [];
 
-   //find all episodes
-   fs.readdir("data", function (err, files) {
-    if (err) {
-        throw err;
-    }
-    
-    files.forEach(function (file) {
-        fileReaded.push(JSON.parse(fs.readFileSync("data/"+file)));
+    //find all episodes
+    fs.readdir("data", function (err, files) {
+        if (err) {
+            throw err;
+        }
+
+        files.forEach(function (file) {
+            fileReaded.push(JSON.parse(fs.readFileSync("data/" + file)));
+        });
+        res.send(fileReaded);
     });
-    res.send(fileReaded);
-  });
 });
 
 router.post('/', function (req, res) {
-  let uuid = uuidv4();
-   fs.writeFile("data/" + "episode" + uuid + ".json", JSON.stringify(req.body), function( error ) {
-      throw error;
-   });
-   res.send(req.body);
+    let uuid = uuidv4();
+    fs.writeFile("data/" + "episode" + uuid + ".json", JSON.stringify(req.body), function (error) {
+        throw error;
+    });
+    res.send(req.body);
 });
 
 router.put('/:id', function (req, res) {
-   let id = req.body.id
-   fs.writeFile("data/" + "episode" + id + ".json", JSON.stringify(req.body), function( error ) {
-      throw error;
-   });
-   res.send(req.body);
+    let id = req.body.id
+    fs.writeFile("data/" + "episode" + id + ".json", JSON.stringify(req.body), function (error) {
+        throw error;
+    });
+    res.send(req.body);
 });
 
 router.get('/:id', function (req, res) {
-   let id = req.param('id');
+    let id = req.param('id');
 
-   //find one episode by id
-   var file = JSON.parse(fs.readFileSync("data/" + "episode" + id + ".json"));
-   
-   res.send(file);
+    //find one episode by id
+    var file = JSON.parse(fs.readFileSync("data/" + "episode" + id + ".json"));
+
+    res.send(file);
+});
+
+router.delete('/:id', function (req, res) {
+    let id = req.param('id');
+
+    fs.unlink("data/" + "episode" + id + ".json", (err) => {
+        if (err) throw err;
+        console.log('successfully deleted /tmp/hello');
+    });
+
+    res.send("deleted");
 });
 
 module.exports = router
